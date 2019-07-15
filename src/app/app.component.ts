8453824764230638ce593b34364ccd9707aa2380
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-07-14 23:43:10 -0400

    s02 assignment 3 - my solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .highlightLog {
+      color: white;
+    }
+  `]
 })
 export class AppComponent {
   title = 'ng8-basics-assignment03';
+  displayDetails = false;
+  clicksLog = [];
+
+  onDisplayDetailsClick() {
+    this.displayDetails = !this.displayDetails;
+    this.clicksLog.push(
+      `button clicked: count=${this.clicksLog.length+1}, time=${new Date()}`);
+  }
 }
